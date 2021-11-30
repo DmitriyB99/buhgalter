64f8acbd252caaf86b597f8e95b69a911d11aa22
unknown <dmitry9989@gmail.com>
2021-11-30 23:21:05 +0600

    contact

--- assets/js/index.js
+++ assets/js/index.js
@@ -22,6 +22,7 @@ let btn = document.querySelector("#btn"),
   colsotr = document.querySelector("#colsotr"),
   colbol = document.querySelector("#colbol"),
   btn3 = document.querySelector("#btn3");
+  // form = document.querySelector("#form");
 
 // Basic function
 btn.onclick = function () {
@@ -131,3 +132,67 @@ btn3.onclick = () => {
 
   out3.innerHTML = sum3;
 };
+
+// form.addEventListener('submit', formSend);
+
+// async function formSend(e) {
+//   e.preventDefault();
+
+//   let error = formValidate(form);
+
+//   let formData = new FormData(form);
+//   if (error === 0) {
+//     form.classList.add('_sending');
+//     let response = await fetch('sendmail.php', {
+//       method: 'POST',
+//       body: formData
+//     });
+//     if (response.ok) {
+//       let result = await response.json();
+//       alert(result.message);
+//       form.reset();
+//       form.classList.remove('_sending');
+//     } else {
+//       alert('Ошибка');
+//       form.classList.remove('_sending');
+//     }
+//   } else {
+//     alert('Заполните обязательные поля');
+//   }
+// }
+
+// function formValidate(form) {
+//   let error = 0;
+//   let formReq = document.querySelector("._req");
+
+//   for (let index = 0; index < formReq.length; index++) {
+//     const input = form[index];
+//     formRemoveError(input);
+
+//     if (input.classList.contains('_email')) {
+//       if (emailTest(input)) {
+//         formAddError(input);
+//         error++;
+//       }
+//     } else {
+//       if (input.value === '') {
+//         formAddError(input);
+//         error++;
+//       }
+//     } 
+//   }
+// }
+
+// function formAddError(input) {
+//   input.parentElement.classList.add('_error');
+//   input.classList.add('_error');
+// }
+
+// function formRemoveError(input) {
+//   input.parentElement.classList.remove('_error');
+//   input.classList.remove('_error');
+// }
+
+// function emailTest(input) {
+//   return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
+// }
